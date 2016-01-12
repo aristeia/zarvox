71123bccd7783c285c9740ac2ce27c3046167590
Aristeia <jsims@pugetsound.edu>
2016-01-12 03:22:29 -0600

    generator now uses album/artist history

--- playlist_generator/getSimilarSong.py
+++ playlist_generator/getSimilarSong.py
@@ -73,7 +73,7 @@ class playlistBuilder:
       return 1
     else:
       i = history.index(type_id)
-      return pow(0.75, (len(history)-1-i))
+      return max(pow(0.75, (len(history)-1-i)), 0.1)
 
   def querySim(self,things_str, thing1, thing2):
     things = eval('self.'+things_str)
