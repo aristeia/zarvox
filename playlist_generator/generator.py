88bb1bfcf91bc075bdffe4914d69cd3480fc826d
Aristeia <jsims@pugetsound.edu>
2016-01-13 14:15:46 -0600

    almost done getting best playlist

--- playlist_generator/generator.py
+++ playlist_generator/generator.py
@@ -1,7 +1,7 @@
 import numpy as np, sys,os, postgresql as pg, bisect, json, whatapi, threading, time
 from random import random, randint 
 import postgresql.driver as pg_driver
-from math import ceil,floor, sqrt
+from math import ceil,floor, sqrt, pow
 from getSimilarSong import playlistBuilder
 sys.path.append("packages")
 from database import databaseCon
@@ -79,7 +79,7 @@ def main():
   genresUsed = db.prepare("SELECT genres.genre, COUNT(subgenre) FROM playlists FULL OUTER JOIN genres ON genres.genre = playlists.subgenre WHERE playlists.genre = $1 GROUP BY genres.genre")
   albumsBest = db.prepare("SELECT album_id, similarity from album_genres where genre_id=$1")
   getSubgenreName = db.prepare("SELECT genres.genre FROM genres WHERE genres.genre_id = $1") 
-  
+  playlistLength = 600
 
   #done with setup; real work now
   while True:
@@ -99,77 +99,87 @@ def main():
       for key,val in subgenres.items()
       if val[1]==genre], key=lambda x: x[1])
     albums = []
-    while len(albums) < 2:
+    while len(albums) < 2 and len(possible_subgenres)>0:
       subgenre, temp = getitem(possible_subgenres)
       possible_subgenres.remove((subgenre,temp))
       albums = sorted([list(lst) for lst in albumsBest(subgenre) if not np.isnan(lst[1])])
-    subgenreName = list(getSubgenreName(subgenre))[0][0]
-    print("Picked "+subgenreName+" as a starting subgenre")
-    albums_rvar = norm(*norm.fit([x[1] for x in albums]))
-    for album in albums:
-      album[1] = albums_rvar.cdf(album[1])
-    album_id, temp = getitem(albums)
-    songs = []
-    minDuration = 0
-    def processNextAlbum(i):
-      album_songs = sorted(
-        eL.processSongs(
-          current_playlist.printAlbumInfo(album_ids[i])),
-        key=lambda x: x.popularity, reverse=True)
-      while len(album_songs) > 5:
-        album_songs.pop()
-      songs.append(album_songs)
-      return min([x.length for x in album_songs])
+    if len(possible_subgenres)==0:
+      print("Error: couldn't find a suitable subgenre for genre")
+    else:
+      subgenreName = list(getSubgenreName(subgenre))[0][0]
+      print("Picked "+subgenreName+" as a starting subgenre")
+      albums_rvar = norm(*norm.fit([x[1] for x in albums]))
+      for album in albums:
+        album[1] = albums_rvar.cdf(album[1])
+      album_id, temp = getitem(albums)
+      songs = []
+      minDuration = 0
+      def processNextAlbum(i):
+        album_songs = sorted(
+          eL.processSongs(
+            current_playlist.printAlbumInfo(album_ids[i])),
+          key=lambda x: x.popularity, reverse=True)
+        while len(album_songs) > 5:
+          album_songs.pop()
+        songs.append(album_songs)
+        return min([x.length for x in album_songs])
 
-    album_ids = [album_id]
-    def getAlbumThread(album_id):
-      album_ids.append(current_playlist.getNextAlbum(album_id))
+      album_ids = [album_id]
+      def getAlbumThread(album_id):
+        album_ids.append(current_playlist.getNextAlbum(album_id))
 
-    while len(album_ids) < 5:
-      getAlbumThread(album_ids[-1])
-    print("Mostly done getting albums")
-    for i in range(5):
-      minDuration+=processNextAlbum(i)
-    print("Mostly done getting song info")
-    while minDuration < 300:
-      getAlbumThread(album_ids[-1])
-      minDuration+=processNextAlbum(len(album_ids)-1)
-    print("All done getting album and song info")
+      while len(album_ids) < 5:
+        getAlbumThread(album_ids[-1])
+      print("Mostly done getting albums")
+      for i in range(5):
+        minDuration+=processNextAlbum(i)
+      print("Mostly done getting song info")
+      while minDuration < playlistLength:
+        getAlbumThread(album_ids[-1])
+        minDuration+=processNextAlbum(len(album_ids)-1)
+      print("All done getting album and song info")
 
-    def assessPlaylist(tracks, length, linerKeys):
-      if len(tracks) == 0 or length >= 300:
-        return [(abs(300-length), [-1 for temp in tracks])]
-      res = []
-      for i in range(len(tracks[0])):
-        l = 0
-        ls = linerKeys[:]
-        if int(linerKeys[0])*60 < length+tracks[0][i].length:
-          l+=min(abs(int(linerKeys[0])*60-length), abs(int(linerKeys[0])*60-length-tracks[0][i].length))
-          ls.pop(0)
-        res.extend([(x+l,[i]+y) for x,y in assessPlaylist(tracks[1:],length+tracks[0][i].length, ls)])
-      if length==0:
-        res.extend([(x,[-1]+y) for x,y in assessPlaylist(tracks[1:],length, linerKeys)])
-      elif length>=260:
-        res.extend([(300-length,[-1 for temp in tracks])])
-      return res
+      def assessPlaylist(tracks, length, linerKeys):
+        if len(tracks) == 0 or length >= playlistLength:
+          return [(abs(playlistLength-length), [-1 for temp in tracks])]
+        res = []
+        for i in range(len(tracks[0])):
+          l = 0
+          ls = linerKeys[:]
+          if int(linerKeys[0])*60 < length+tracks[0][i].length:
+            l+=min(abs(int(linerKeys[0])*60-length), abs(int(linerKeys[0])*60-length-tracks[0][i].length))
+            ls.pop(0)
+          res.extend([(x+l,[i]+y) for x,y in assessPlaylist(tracks[1:],length+tracks[0][i].length, ls)])
+        if length==0:
+          res.extend([(x,[-1]+y) for x,y in assessPlaylist(tracks[1:],length, linerKeys)])
+        elif length + pow(playlistLength,0.4) >= playlistLength:
+          res.extend([(playlistLength-length,[-1 for temp in tracks])])
+        return res
 
-    playlists = assessPlaylist(songs, 0, list(linerTimes.keys()))
-    print("Done getting playlist info")
-    
-    print(playlists)
-    print('\n'.join([','.join([s[z].name+' '+str(s[z].length) for z,s in zip(y,songs) if z>=0  ]) for x,y in playlists]))
-    playlists.sort(key=lambda x: x[0])
-    p = playlists[0]
-    print(p[0], ', '.join([x.name for x in p[1]]))
+      playlists = assessPlaylist(songs, 0, list(linerTimes.keys()))
+      print("Done getting playlist info")
+      
+      print(playlists)
+      print('\n'.join([','.join([s[z].name+' '+str(s[z].length) for z,s in zip(y,songs) if z>=0  ]) for x,y in playlists]))
+      playlists.sort(key=lambda x: x[0])
+      p = playlists[0]
+      print(p[0], ', '.join([x.name for x in p[1]]))
 
-    playlistEval = lambda x: sum([y for y in x[1] if y > 0])
-    i = 1
-    while playlists[0][0] < 15*i:
-      i+=1
-    bestPlaylist = min([p for p in playlists if p[0] < 15*i], key=playlistEval)
-    print("Best Playlist:")
-    print(bestPlaylist[0], ', '.join([x.name+' '+str(x.length) for x in bestPlaylist[1]]))
+      playlistEval = lambda x: sum([y for y in x[1] if y > 0])
+      i = 1
+      while playlists[0][0] < 15*i and i<20:
+        i+=1
+      if i==20:
+        print("Error with playlists: all have bad timing with respect to liners")
+        print("Best timing: "+str(playlists[0][0]))
+      else:
+        bestPlaylist = min([p for p in playlists if p[0] < 15*i], key=playlistEval)
+        print("Best Playlist:")
+        print(bestPlaylist[0], ', '.join([x.name+' '+str(x.length) for x in bestPlaylist[1]]))
     exit(0)
+    hour = (hour + 1)% 23
+    if hour==0:
+      day = (day + 1) % 6
 
 
 if  __name__ == '__main__':
