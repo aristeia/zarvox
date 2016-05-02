1a1f31b72753c2b9674c57252b565d7c615e5ef6
Jon Simz <jsims@pugetsound.edu>
2016-05-02 14:15:10 -0700

    various bug fixes and error handling

--- postprocessor/albumImporter.py
+++ postprocessor/albumImporter.py
@@ -5,10 +5,12 @@ Wrapper script for generate metadata & postprocessor
 import sys,os
 sys.path.extend(os.listdir(os.getcwd()))
 import generateMetadata, postprocessor
+from libzarv import getConfig
 
 def albumImport(directoryPath = None, albumsFolder = None):
     if directoryPath is None:
         directoryPath = sys.argv[1]
+        albumsFolder = getConfig()['albums_folder']
         postprocessor.main(False)
     directoryPath = directoryPath.strip('/')
     generateMetadata.checkoutFolder(albumsFolder+'/'+directoryPath+'/')
