876cdc43f179c25803da949bed0ff1c722c68fa5
Jon Simz <jsims@pugetsound.edu>
2016-05-02 15:42:16 -0700

    default song filename is none

--- downloader/lookup.py
+++ downloader/lookup.py
@@ -106,7 +106,7 @@ def songLookup(metadata,song,path,con=None):
     lastfm_listeners = 0
     lastfm_playcount = 0
     kups_playcount = 0
-    if all(len(x)>0 for x in [path, metadata['path_to_album']])
+    if all(len(x)>0 for x in [path, metadata['path_to_album']]):
       path_to_song = metadata['path_to_album']+'/'+path
     else:
       path_to_song = ''
