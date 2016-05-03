d02a7dd6cf5408192d2f3e5c752df09ce606bb21
Jon Simz <jsims@pugetsound.edu>
2016-05-03 07:59:21 -0700

    finally fixed illegial char issue for all

--- postprocessor/directoryImporter.py
+++ postprocessor/directoryImporter.py
@@ -2,16 +2,18 @@
 Wrapper script for albumImporter for all directories in album folder
 
 '''
-import sys,os,json
+import sys,os,json,re
 sys.path.extend(os.listdir(os.getcwd()))
 from albumImporter import albumImport
 import postprocessor
-from libzarv import getConfig, handleError
+from libzarv import getConfig, handleError, getIllegalChars
 from random import shuffle
 
 root_dir = getConfig()["albums_folder"]
 metadata_filename = root_dir+"/.metadata.json"
+illegalCharRE = getIllegalChars()
 postprocessor.main(False)
+
 
 if os.path.isfile(metadata_filename):
     with open(metadata_filename,"r") as f:
@@ -28,14 +30,14 @@ try:
     totalDirs = str(len(lstdir))
     shuffle(lstdir)
     for path in lstdir:
-        if u'\udcb0' not in path and u'\udcf6' not in path and path not in directories:
+        if illegalCharRE.match(path) is None and path not in directories:
             directories.append(path)
             try:
                 print("Importing '"+path+"'")
                 albumImport(path, root_dir)
             except Exception as e:
                 handleError(e, "Warning: issue with directory '"+path+"'")
-            if len(directories) % 25 == 0:
+            if len(directories) % 10 == 0:
                 writeToFile()
             print("Done with folder "+str(len(directories))+"/"+totalDirs)
 except Exception:
